6a79f395f7a0e99476f2537ca8fedf548414e9fa
ei1125 <i.dont.like.monday1125@gmail.com>
2020-05-12 19:10:30 +0900

    修正

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker-compose -p project-name -f /var/www/asset-station/docker-compose.yml down
+docker-compose -f /var/www/asset-station/docker-compose.yml down
 
-docker-compose -p project-name -f /var/www/asset-station/docker-compose.yml up -d
+docker-compose -f /var/www/asset-station/docker-compose.yml up -d
